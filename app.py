20962b0cd10893b41eadfe460dd6c6424cf7b036
Alexander Immer <ext-alexander.immer@here.com>
2016-05-25 01:48:52 +0200

    Remove old api endpoints

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask import make_response
 from flask_restful import Api
 
-from app.handlers import Recipes, Session, Match, InitUser, NearByUsers
+from app.handlers import Recipes, RecipeCard
 
 
 app = Flask(__name__, static_url_path="")
@@ -14,10 +14,7 @@ def not_found(_):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 api.add_resource(Recipes, '/recipes-from-ingredients')
-api.add_resource(Session, '/session')
-api.add_resource(Match, '/match')
-api.add_resource(InitUser, '/user')
-api.add_resource(NearByUsers, '/nearby')
+api.add_resource(RecipeCard, '/recipe-card')
 
 
 if __name__ == "__main__":
