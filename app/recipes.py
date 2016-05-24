784f99e6ac482f49b4205d478d8ca70b3b7bd99b
Alexander Immer <ext-alexander.immer@here.com>
2016-05-25 01:49:54 +0200

    Extend recipe extraction methods

--- app/recipes.py
+++ app/recipes.py
@@ -17,40 +17,51 @@ class RecipeProvider:
             'ingredients': ','.join(ingredients),
             'limitLicense': False,
             'ranking': 2,
-            'number': 1000
+            'number': 1000,
+            'fillIngredients': True
         }
         return params
 
     @classmethod
     def best_n_recipes(cls, ingredients, n=100):
+        required = ['recipe_id', 'title', 'missing_ingredients', 'image_url', 'ingredients']
         params = cls.ranking_params(ingredients)
         recipes = requests.get(cls.base_url + '/recipes/findByIngredients',
                                headers=cls.headers, params=params).json()
         if not recipes:
-            return None
+            return []
+
         recipes.sort(key=lambda r: r.get('likes'), reverse=True)
-        return recipes[:100]
+        recipes = recipes[:n]
+        for i, recipe in enumerate(recipes):
+            recipe['ingredients'] = recipe.get('missedIngredients', []) + \
+                                    recipe.get('usedIngredients', [])
+            recipe['image_url'] = recipe['image']
+            recipe['recipe_id'] = recipe['id']
+            recipe['missing_ingredients'] = recipe.get('missedIngredients', [])
+            recipes[i] = {r: recipe[r] for r in required}
+        return recipes
 
     @classmethod
     def full_recipe(cls, _id):
         summary = cls.recipe_summary(_id)
-        information = cls.recipe_information(_id)
+        information = cls.recipe_extended_ingredients(_id)
         summary.update(information)
         return summary
 
     @classmethod
-    def recipe_information(cls, _id):
-        required = ['summary']
+    def recipe_extended_ingredients(cls, _id):
+        required = ['extendedIngredients']
         params = {
             'includeNutrition': False
         }
         res = requests.get(cls.base_url + '/recipes/{}/information'.format(_id),
-                           readers=cls.headers, params=params).json()
+                           headers=cls.headers, params=params).json()
         return {key: res[key] for key in required}
 
     @classmethod
     def recipe_summary(cls, _id):
-        required = ['id', 'extendedIngredients', 'title', 'image']
+        required = ['summary']
         params = {
             'includeNutrition': False
         }
